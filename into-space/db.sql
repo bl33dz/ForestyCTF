11bbf1a5100f3af04f93a53abf20159a4f8546c0
bl33dz <fcb775566@gmail.com>
2022-08-24 15:14:09 +0700

    new chall

---
+++ into-space/db.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.19  Distrib 10.3.34-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: into_space
+-- ------------------------------------------------------
+-- Server version	10.3.34-MariaDB-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(40) NOT NULL,
+  `upload_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'edffe344f828b71febd8a3ca220c8ea7.jpg','2022-08-23'),(2,'36fcd6cecc1441e8f859a4bc44f9f97d.png','2022-08-23');
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-23 19:11:31
---
+++ into-space/src/index.php
@@ -0,0 +1,191 @@
+<?php
+
+define("MAX_SIZE", 100000); // 100 kb
+define("DIR", "space/");
+
+$servername = "db";
+$username = "root";
+$password = "justdummypass1337";
+$dbname = "into_space";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+function upload_image($file) {
+  $fname = basename($file["name"]);
+  $type = strtolower(pathinfo($fname, PATHINFO_EXTENSION));
+  $name = hash("md5", $fname.time()) . "." . $type;
+  $path = DIR . $name;
+  $status = 1;
+  $message = "";
+
+  if($type != "jpg" && $type != "png" && $type != "jpeg") {
+    $status = 0;
+    $message = "Unsupported file type detected.";
+    goto end;
+  }
+
+  if($file["size"] > MAX_SIZE) {
+    $status = 0;
+    $message = "File is too large.";
+    goto end;
+  }
+
+  if(getimagesize($file["tmp_name"] == false)) {
+    $status = 0;
+    $message = "Not an image file.";
+  }
+
+  end:
+  if ($status == 0) {
+    return "ERROR: " . $message;
+  } else {
+    $x = insert($name);
+    if (move_uploaded_file($file["tmp_name"], $path) && $x === true) {
+      return "File ". $name ." uploaded.";
+    } else {
+      return "If this happen, report to problem setter.".$x;
+    }
+  }
+}
+
+function insert($filename) {
+  global $conn;
+  $sql = "INSERT INTO images (filename, upload_date) VALUES ('".$filename."', CURDATE())";
+  if($conn->query($sql)) {
+    return true;
+  } else {
+    return $conn->error;
+  }
+}
+
+function list_uploaded() {
+  global $conn;
+  $sql = "SELECT id, filename, upload_date FROM images";
+  $result = $conn->query($sql);
+  return $result;
+}
+
+function view_image($id) {
+  global $conn;
+  $id = base64_decode($id);
+  $sql = "SELECT filename, upload_date FROM images WHERE id = ".$id;
+  $result = $conn->query($sql);
+  return $result;
+}
+
+?>
+<html>
+<head>
+  <title>into-space</title>
+  <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+  <style>
+  .center {
+      text-align: center;
+  }
+  .img {
+      margin-bottom: 25px;
+  }
+  :root {
+      --accent: #4B07D3;
+  }
+  .custom-file-upload {
+    border: 1px solid #4B07D3;
+    border-radius: 0.3rem;
+    display: inline-block;
+    padding: 8px 12px;
+    cursor: pointer;
+  }
+  input[type="file"] {
+    display: none;
+  }
+  table {
+      table-layout: fixed;
+    }
+  tr, td, th {
+    border: 0px;
+    border-bottom: 2px solid #4B07D3;
+  }
+  .fid {
+    width: 70px;
+  }
+  .fname {
+    width: 380px;
+  }
+  .fdate {
+    width: 150px;
+  }
+  .faction {
+    width: 55px;
+  }
+  </style>
+</head>
+<body>
+  <script>
+  function uploadFile(target) {
+    document.getElementById("filename").innerHTML = target.files[0].name;
+  }
+  </script>
+  <div class=center>
+  <h1>into-space</h1>
+  <div style="height: 350px">
+    <img id=img class=img src="space/space.gif" style="max-height: 100%; max-width: 100%"><br>
+  </div>
+  <p>[ <a href=?upload>Upload</a> ] - [ <a href=?list>List</a> ]</p>
+    <?php
+    if(isset($_GET['upload'])) {
+    ?>
+    <form method="post" enctype="multipart/form-data">
+      <p>Upload your image to space (max: 100 kB)</p>
+      <label class="custom-file-upload">
+        <input type="file" name="upload" onchange="uploadFile(this)">
+        <text id=filename>No File Choose</text>
+      </label>
+      <input type="submit" value="Upload Image" name="submit">
+      <p><?php
+      if(isset($_POST['submit'])) {
+        echo upload_image($_FILES["upload"]);
+      }
+    ?></p>
+    </form>
+    <?php
+    } elseif(isset($_GET['list'])) {
+    ?>
+    <table class=border>
+      <tr>
+        <th class=fid>File ID</th>
+        <th class=fname>Filename</th>
+        <th class=fdate>Upload Date</th>
+        <th class=faction>Action</th>
+      </tr>
+      <?php
+      $list = list_uploaded();
+      if($list->num_rows > 0) {
+        while($row = $list->fetch_assoc()) {
+      echo "<tr>
+        <td>".$row['id']."</td>
+        <td>".$row['filename']."</td>
+        <td>".$row['upload_date']."</td>
+        <td><a href=?view=".base64_encode($row['id']).">View</a></td>
+      </tr>";
+        }
+      }
+      ?>
+    </table>
+    <?php
+    } elseif(isset($_GET['view'])) {
+      $data = view_image($_GET['view']);
+      $data = $data->fetch_row();
+      echo "
+    ".$data[0]." (".$data[1].")
+    <script>
+    document.getElementById('img').src='space/".$data[0]."';
+    </script>";
+    }
+    ?>
+  </div>
+</body>
+</html>
